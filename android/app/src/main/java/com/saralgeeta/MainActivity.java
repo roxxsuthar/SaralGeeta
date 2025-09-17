3024ac064d6f6f7db134b604b8aad26a1feb58a3
BHAGIRATH SUTHAR <bhagirathsuthar@BHAGIRATHs-MacBook-Air.local>
2025-09-17 09:31:20 +0530

    fix the build issue

--- android/app/src/main/java/com/saralgeeta/MainActivity.java
+++ android/app/src/main/java/com/saralgeeta/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new DefaultReactActivityDelegate(this, getMainComponentName());
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                false // fabric enabled
+        );
     }
 }
