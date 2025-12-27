92e2fa01408f9dee726762a8434dc7c7b9a8a859
Dushyant Jaidka <dushyantjaidka@Dushyants-MacBook-Air.local>
2025-12-28 04:56:38 +0530

    Arch upgrade

--- android/app/src/main/java/com/saralgeeta/MainApplication.kt
+++ android/app/src/main/java/com/saralgeeta/MainApplication.kt
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
@@ -28,10 +29,6 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
-        
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            DefaultNewArchitectureEntryPoint.load()
-        }
+        loadReactNative(this)
     }
 }
